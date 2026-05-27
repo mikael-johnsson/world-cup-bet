571422f048248c052b7a522d923ed8764aea6b22
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2026-05-27 09:04:14 +0200

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import { AuthProvider } from "@/context/AuthContext";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Mickes VM-tips",
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Header />
           {children}
         </AuthProvider>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="flex flex-col gap-4 bg-green-700 text-green-500 p-6 items-center">
+      <p className="text-center">
+        Problem med sidan? Maila mikaeljohanjohnsson [at] gmail.com
+      </p>
+      <p className="text-center">&copy; Mikael Johnsson 2026</p>
+    </div>
+  );
+};
+
+export default Footer;
